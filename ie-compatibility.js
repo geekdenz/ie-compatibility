955e948e4f04f35cb7178c49ed1a91006422f046
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2014-09-30 16:33:51 +1300

    corrected typo

--- ie-compatibility.js
+++ ie-compatibility.js
@@ -2,7 +2,7 @@
 var content = '<div id="ie-compatibility-popover" style="z-index: 9999; position: absolute; width: 200px; height: 100px;'+
 'top: 0; right: 0px; background-color: #777; color: white; padding: 16px;'+
 'text-align: center">'+
-'  <p>For correct display of this site, please turn of "Compatibility View".</p>'+
+'  <p>For correct display of this site, please turn off "Compatibility View".</p>'+
 '  <p>You can do this by clicking on the "broken page" symbol next to the'+
 '     address bar in Internet Explorer.</p>'+
 '  <a href="#" id="ie-compatibility-close" style="background-color: white;'+
